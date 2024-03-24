cfc08485683cb4f519a1c198b3eafc251f7fbffb
eztornado <ivanps94@protonmail.com>
2024-03-24 19:15:32 +0100

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('active')->default(0);
-            $table->integer('roles_id');
+            $table->text('remember_token');
             $table->timestamps();
         });
     }
